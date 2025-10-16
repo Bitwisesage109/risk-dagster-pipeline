8750ebd5dc965c0ad7c882da4316995c5600d8d1
Bitwisesage109 <rashid.husain@sosuvconsulting.com>
2025-10-16 11:53:13 -0400

    Initial commit: Migrating Risk pipeline from shared path to Git

--- setup.py
+++ setup.py
@@ -1,11 +1,19 @@
-from setuptools import find_packages, setup
+from setuptools import setup, find_packages
 
 setup(
     name="Risk",
+    version="0.1.0",
     packages=find_packages(exclude=["Risk_tests"]),
     install_requires=[
         "dagster",
-        "dagster-cloud"
+        "dagster-cloud",
+        "pandas",
+        "pyodbc",
+        "paramiko",
+        "cryptography",
     ],
-    extras_require={"dev": ["dagster-webserver", "pytest"]},
+    author="Bitwisesage109",
+    description="Risk Pipeline Dagster Project",
+    python_requires=">=3.8",
 )
+
